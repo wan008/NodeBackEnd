f95cbba24736e271faa44e2001d16afb52e9bc2a
wan008 <kimw@sheridancollege.ca>
2024-01-25 17:41:55 -0500

    add javascript to front file

--- app/app5.js
+++ app/app5.js
@@ -13,6 +13,9 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+console.log(`${__dirname}`);
+app.use(express.static(`${__dirname}/src/public`));
+
 app.use("/", home); // use -> method to register middle ware
 
 module.exports = app;
